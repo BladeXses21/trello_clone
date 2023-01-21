59cde5ed8980fea992dadfac4b4d0175653a15a5
BladeXses21 <bladexsesamv@gmail.com>
2023-01-21 14:06:00 +0200

    'install react-rudux to project, add todos bord, added function "getTodos"'

---
+++ frontend/src/components/todo/List.js
@@ -0,0 +1,48 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { getTodos } from '../../actions/todos';
+
+class List extends Component {
+
+    static propTypes = {
+        todos: PropTypes.array.isRequired
+    };
+
+    componentDidMount() {
+        this.props.getTodos();
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <h2>Todo list</h2>
+                <table className='table table-striped'>
+                    <thead>
+                        <tr>
+                            <th>title</th>
+                            <th>description</th>
+                            <th>done</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.todos.map(todo => (
+                            <tr key={todo.id}>
+                                <td>{todo.title}</td>
+                                <td>{todo.description}</td>
+                                <td><input type='checkbox' defaultChecked={todo.done} /></td>
+                                <td><button className='btn btn-danger btn-sm'>Delete</button></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </Fragment>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    todos: state.todos.todos
+})
+export default connect(mapStateToProps, { getTodos })(List);
---
+++ frontend/src/reducers/todos.js
@@ -0,0 +1,17 @@
+import { GET_TODO_LIST, ADD_TODO, DELETE_TODO, TOGGLE_TODO } from '../actions/types';
+
+const initialState = {
+    todos: []
+};
+
+export default function (state = initialState, action){
+    switch (action.type) {
+        case GET_TODO_LIST:
+            return {
+                ...state,
+                todos: action.payload
+            }
+        default:
+            return state;
+    }
+}
